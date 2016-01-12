f0492ee22f8a0073b583404813c59a4526f4d677
JM Han <13240943@qq.com>
2016-01-12 10:35:48 -0800

    update script

---
+++ python/CPP/c08/if.py
@@ -0,0 +1,9 @@
+balance = 1.0
+key     = 2.0
+if balance > 0.0:
+    if key > 3.0:
+        print "key is greater than 3.0"
+    #else:
+    #    print "key is less than 3.0"
+else:
+    print "balance is greater than 1.0"
