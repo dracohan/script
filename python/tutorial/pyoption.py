6e79c97840fcc2a886567f11282bc1cc307ca198
JM Han <13240943@qq.com>
2016-03-11 17:39:59 +0800

    update script@office

--- python/tutorial/pyoption.py
+++ python/tutorial/pyoption.py
@@ -1,5 +1,12 @@
+#!/usr/bin/env python
+
+import os
+
 def callfrommain():
     print("call from main")
+    print '__file__ is: ',  __file__
+    print '__package__ is: ',  __package__
 
+#thisismain
 if __name__ == '__main__':
     callfrommain()
